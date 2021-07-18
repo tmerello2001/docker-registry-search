d099d2ada6495c7eb69d6760fd9208471c9bfd40
Tomas Merello <tomas@merello.net>
2021-07-18 03:26:17 -0300

    Add small install script

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ "$1" = "--short" ]; then
+    mv docker-registry-search /usr/local/bin/drs
+else
+    if [ "$1" = "-short" ]; then
+        mv docker-registry-search /usr/local/bin/drs
+    else
+        mv docker-registry-search /usr/local/bin
+    fi
+fi
