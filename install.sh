fe1bbda565606c48ce6aff4dcacb611e337827a9
Tomas Merello <tomas_merello@hotmail.com>
2021-07-18 03:56:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-if [ "$1" = "--short" ]; then
-    mv docker-registry-search /usr/local/bin/drs
-else
-    if [ "$1" = "-short" ]; then
-        mv docker-registry-search /usr/local/bin/drs
-    else
-        mv docker-registry-search /usr/local/bin
-    fi
-fi
+mv docker-registry-search /usr/local/bin
